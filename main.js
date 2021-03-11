f5f90473a4965501cefba7e1379c5fe0b7cf48dc
Oscar Asikoyo <64526213+95asikoyo@users.noreply.github.com>
2021-03-11 20:37:22 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 1000,
+    reset: true
+});
+
+
+sr.reveal(`#embo, #blue, #rembo, #green, .row mobile_pad1, .cert, #footer`, {
+    interval: 100
+})
